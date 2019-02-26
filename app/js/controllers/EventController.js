dccf85cac96e4472c637fb302a77548aded47800
Lindsey Burdette <lindsey.burdette@allstar.technology>
2019-02-26 08:51:07 -0500

    Switching from asp.net server to node.js server.

---
+++ app/js/controllers/EventController.js
@@ -0,0 +1,11 @@
+'use strict';
+
+eventsApp.controller('EventController',
+function EventController($scope){
+  $scope.event={
+    name:'Angular boot camp',
+    date:'2/2/2019',
+    time:'3:00pm'
+  }
+}
+);
